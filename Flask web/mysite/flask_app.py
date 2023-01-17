e2372a9fd9b0ad2972e477a965f93366bf196fb5
Joe Duthu <64978431+duthu@users.noreply.github.com>
2023-01-17 12:57:46 +0300

    basis

--- Flask web/mysite/flask_app.py
+++ Flask web/mysite/flask_app.py
@@ -22,7 +22,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-uploadPath = "/home/chromeservices/mysite/uploads"
+
 app.config['UPLOAD_FOLDER'] = uploadPath
 
 db = SQLAlchemy(app)
